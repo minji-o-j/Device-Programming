368eea25e9d0f69696f2aa3d565b3aa28d5b32d1
minji-o-j <jminji98@gmail.com>
2020-06-07 17:19:59 +0900

    Revert "Initial commit"
    This reverts commit f4c7ecab05070a0889af1e93bb79fab2da8d3733.

--- etc/rom_example2_inst.vhd
+++
@@ -1,5 +0,0 @@
-rom_example2_inst : rom_example2 PORT MAP (
-		address	 => address_sig,
-		clock	 => clock_sig,
-		q	 => q_sig
-	);
--- etc/rom_example_inst.vhd
+++
@@ -1,5 +0,0 @@
-rom_example_inst : rom_example PORT MAP (
-		address	 => address_sig,
-		clock	 => clock_sig,
-		q	 => q_sig
-	);
